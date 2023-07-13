2ecbc045e8859394f2cc2ff621a25be6cac44de8
sukij <sukij44800@gmail.com>
2023-07-14 02:52:02 +0700

    third commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -28,11 +28,12 @@ export default function Home() {
         quantity={100}
       /> */}
       <h1 className="flex flex-col items-center z-10 animate-text-fade-in text-8xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display whitespace-nowrap bg-clip-text ">
-        Skooldio's Hackathon 2023
+        The 2023 Skooldio Hackathon
       </h1>
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
         <h2 className="text-sm text-zinc-200">Group 2 Proudly Present</h2>
+        <h2 className="text-sm text-zinc-500 mt-2">Big Thanks to ChatGPT</h2>
       </div>
     </div>
   );
